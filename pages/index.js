48c7e811aec4c2c296438678b3a482214db57baf
Spook <spook.devv@gmail.com>
2024-04-12 09:46:47 -0400

    NextJS PreRenders by default - added getStaticProps to pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
 
+// this function  will be executed first then the react component function
+// This prepares the props data
+// fetches and exposes data through props to the component function
+// can do server side things
+// can use credentials that users shouldn't see
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: 1, title: "Milk" }],
+    },
+  };
+}
 export default HomePage;
