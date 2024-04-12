ebc6b0ec44b88978179445d19e6cdfd1131a330e
Spook <spook.devv@gmail.com>
2024-04-12 09:53:03 -0400

    Running server side code and using the File System

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from "fs/promises";
+import path from "path";
+
 function HomePage(props) {
   const { products } = props;
   return (
@@ -15,9 +18,13 @@ function HomePage(props) {
 // can do server side things
 // can use credentials that users shouldn't see
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // cwd is treats all files as being in the root directory
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 1, title: "Milk" }],
+      products: data.products,
     },
   };
 }
